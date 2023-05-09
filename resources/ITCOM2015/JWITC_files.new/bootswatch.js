ea5e5cc7aabadc318eaff8a625673f084e5a4dae
intitni <intitni@users.noreply.github.com>
2023-05-09 14:37:34 +0000

    deploy: cheewtan/home@744607f4c6592b91b29c121fcc68137b95bb6d59

--- resources/ITCOM2015/JWITC_files.new/bootswatch.js
+++ resources/ITCOM2015/JWITC_files.new/bootswatch.js
@@ -1,3 +1,42 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:48be760933084271d262c6fa5af63c4f1d9bdd491b995f6a1deb395af3b680ec
-size 948
+(function(){
+
+  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
+    var html = $(this).parent().html();
+    html = cleanSource(html);
+    $("#source-modal pre").text(html);
+    $("#source-modal").modal();
+  });
+
+  $('.bs-component [data-toggle="popover"]').popover();
+  $('.bs-component [data-toggle="tooltip"]').tooltip();
+
+  $(".bs-component").hover(function(){
+    $(this).append($button);
+    $button.show();
+  }, function(){
+    $button.hide();
+  });
+
+  function cleanSource(html) {
+    var lines = html.split(/\n/);
+
+    lines.shift();
+    lines.splice(-1, 1);
+
+    var indentSize = lines[0].length - lines[0].trim().length,
+        re = new RegExp(" {" + indentSize + "}");
+
+    lines = lines.map(function(line){
+      if (line.match(re)) {
+        line = line.substring(indentSize);
+      }
+
+      return line;
+    });
+
+    lines = lines.join("\n");
+
+    return lines;
+  }
+
+})();
